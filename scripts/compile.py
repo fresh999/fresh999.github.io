38f3b302f2c256936c64a5a15fb0c19e62d7cad0
fresh999 <francirusc@gmail.com>
2021-08-02 18:11:02 +0200

    Compiler does not support nested environments (look into recursive method)

--- scripts/compile.py
+++ scripts/compile.py
@@ -2,14 +2,25 @@
 
 import numpy as np
 
+thm_envs = ['theorem', 'proposition', 'lemma', 'proof']
+math_envs = ['equation', 'align']
+list_envs = ['enumerate']
+
 # block is a list of strings
 def is_math_block(block):
-    math_openers = ['\\begin{equation', '\\begin{align']
     for line in block:
-        for o in math_openers:
-            if o in line:
+        for env in math_envs:
+            if f'\\begin{{{env}' in line:
                 return True
     return False
+
+# block is a list of strings
+def find_thm_type(chunk):
+    for line in chunk:
+        for env in thm_envs:
+            if f'\\begin{{{env}}}' in line:
+                return env
+    return None
 
 class Block:
     # lines is a list of strings
@@ -24,6 +35,50 @@ class Block:
         else:
             self.lines = ['<p>' + line.rstrip() + '</p>\n' if not line == '\n' else line for line in self.lines]
 
+class PostChunk:
+    def __init__(self, lines, thm_type=None):
+        self.lines = lines
+        self.thm_type = thm_type
+
+    def find_math_envs(self):
+        beg = []
+        end = []
+        for i, line in enumerate(self.lines):
+            for env in math_envs:
+                if f'\\begin{{{env}' in line:
+                    beg.append(i)
+                if f'\\end{{{env}' in line:
+                    end.append(i+1)
+        return [i for tup in zip(beg, end) for i in tup]
+
+    def parse_chunk(self):
+        indices = self.find_math_envs()
+        blocks = [sl.tolist() for sl in np.split(self.lines, indices)]
+        self.blocks = [Block(block, is_math_block(block)) for block in blocks]
+
+    def insert_tags(self):
+        if self.thm_type:
+            for block in self.blocks[1:-2]:
+                block.insert_tags()
+        for block in self.blocks:
+            block.insert_tags()
+
+    def delimit_chunk(self):
+        if self.thm_type:
+            for line in self.blocks[0].lines:
+                if f'\\begin{{{self.thm_type}}}' in line:
+                    self.blocks[0].lines.remove(line)
+            for line in self.blocks[-1].lines:
+                if f'\\end{{{self.thm_type}}}' in line:
+                    self.blocks[-1].lines.remove(line)
+            self.blocks.append(Block(['</div>\n']))
+            self.blocks.insert(0, Block([f'<div class="{self.thm_type}">\n']))
+
+    def compile_chunk(self):
+        blocks = self.parse_chunk()
+        self.insert_tags()
+        self.delimit_chunk()
+
 class Post:
     def __init__(self, post_filename):
         with open(post_filename, 'r') as post:
@@ -33,65 +88,46 @@ class Post:
         beg_ix = 0
         end_ix = 0
         for i, line in enumerate(self.lines):
-            if line == '\\begin{document}\n':
+            if '\\begin{document}' in line:
                 beg_idx = i+1
-            if line == '\\end{document}\n':
+            if '\\end{document}' in line:
                 end_idx = i
         return (beg_idx, end_idx)
 
     def get_post_lines(self):
         beg_ix, end_ix = self.get_beg_end_indices()
         self.lines = self.lines[beg_ix : end_ix]
-        while '\n' in self.lines:
-            self.lines.remove('\n')
+#        this gets rid of blank lines
+#        self.lines = [line for line in self.lines if line.strip()]
 
-    def find_math_envs(self):
-        math_openers = ['\\begin{equation', '\\begin{align']
-        math_closers = ['\\end{equation', '\\end{align']
+    def find_thm_envs(self):
         beg = []
         end = []
         for i, line in enumerate(self.lines):
-            for o in math_openers:
-                if o in line:
+            for env in thm_envs:
+                if f'\\begin{{{env}}}' in line:
                     beg.append(i)
-            for c in math_closers:
-                if c in line:
+                if f'\\end{{{env}}}' in line:
                     end.append(i+1)
         return [i for tup in zip(beg, end) for i in tup]
 
     def parse_post(self):
-        indices = self.find_math_envs()
-        blocks = [sl.tolist() for sl in np.split(self.lines, indices)]
-        return [Block(block, is_math_block(block)) for block in blocks]
-
-    def insert_tags(self, blocks):
-        for block in blocks:
-            block.insert_tags()
-        return blocks
+        indices = self.find_thm_envs()
+        chunks = [sl.tolist() for sl in np.split(self.lines, indices)]
+        return [PostChunk(chunk, find_thm_type(chunk)) for chunk in chunks]
 
     def compile_post(self):
         self.get_post_lines()
-        blocks = self.parse_post()
-        return self.insert_tags(blocks)
-
-    def find_thm_envs(self):
-        thm_openers = ['\\begin{theorem}', '\\begin{proposition}', '\\begin{lemma}']
-        thm_closers = ['\\end{equation}', '\\end{proposition}' '\\end{lemma}']
-        beg = []
-        end = []
-        for i, line in enumerate(self.lines):
-            for o in thm_openers:
-                if o in line:
-                    beg.append(i)
-            for c in thm_closers:
-                if c in line:
-                    end.append(i)
-        return zip(beg, end)
+        chunks = self.parse_post()
+        for chunk in chunks:
+            chunk.compile_chunk()
+        blocks = [block for chunk in chunks for block in chunk.blocks]
+        return blocks
 
 
 if __name__ == '__main__':
 
-    post = Post('../latex_posts/mathematical_induction.tex')
+    post = Post('../latex_posts/article.tex')
     blocks = post.compile_post()
 
     pars = [line for block in blocks for line in block.lines]

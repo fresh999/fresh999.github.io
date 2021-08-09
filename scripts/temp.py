e964f5b98561852ebf0c41d8260dee34dbb953c4
fresh999 <francirusc@gmail.com>
2021-08-09 13:07:43 +0200

    I think I have to switch to trees

--- scripts/temp.py
+++ scripts/temp.py
@@ -10,12 +10,11 @@ list_envs = ['enumerate']
 math_envs = ['equation', 'align']
 thm_envs = ['theorem', 'proposition', 'lemma', 'proof']
 
-def get_outer_subblocks(lines):
+def find_outer_blocks(lines):
     indices = []
     env_count = 0
     for i, line in enumerate(lines):
-        match = opening_p.match(line)
-        if match:
+        if opening_p.match(line):
             env_count += 1
             if env_count == 1:
                 indices.append(i)
@@ -23,9 +22,7 @@ def get_outer_subblocks(lines):
             env_count -= 1
             if env_count == 0:
                 indices.append(i+1)
-    blocks = [sl.tolist() for sl in np.split(lines, indices)]
-    blocks = list(filter(([]).__ne__, blocks))
-    return [Block(b) for b in blocks]
+    return indices
 
 def get_post_lines(lines):
     start = 0
@@ -40,6 +37,7 @@ def get_post_lines(lines):
 class Block:
     def __init__(self, lines):
         self.lines = lines
+        self.type = self.get_block_type()
 
     def get_block_type(self):
         if not self.lines:
@@ -47,12 +45,22 @@ class Block:
         pattern = re.compile(r'^\s*\\begin\{(.*)\}')
         m = pattern.match(self.lines[0])
         if m:
-            self.lines = self.lines[1:-1]
             return m.group(1)
 
+    def get_subblocks(self):
+        indices = find_outer_blocks(self.lines)
+        blocks = [sl.tolist() for sl in np.split(self.lines, indices)]
+        blocks = list(filter(([]).__ne__, blocks))
+        return [Block(b) for b in blocks]
+
     def process_block(self):
-        subblocks = get_outer_subblocks(self.lines)
-        self.type = self.get_block_type()
+        if self.type in thm_envs:
+            print('Fuck you')
+#            re.sub(r'^\s*\\begin\{(.*)\}', r'\<div class="\1"\>', self.lines[0])
+#            re.sub(r'^\s*\\end\{(.*)\}', r'\</div>', self.lines[-1])
+            self.lines[0] = f'<div class="{self.type}">\n'
+            self.lines[-1] = '</div>\n'
+        subblocks = self.get_subblocks()
         if len(subblocks) == 1:
             self.insert_tags()
             return self.lines
@@ -61,12 +69,9 @@ class Block:
     def insert_tags(self):
         if not self.type:
             self.lines = ['<p>' + line.rstrip() + '</p>\n' for line in self.lines if not re.search(r'^\s*\n$', line)]
-        elif self.type in thm_envs:
-            self.lines.insert(0, f'<div class="{self.type}">\n')
-            self.lines.append('</div>\n')
         elif self.type in list_envs:
-            self.lines.insert(0, '<ol>\n')
-            self.lines.append('</ol>\n')
+            self.lines[0] = '<ol>\n'
+            self.lines[-1] = '</ol>\n'
             self.lines = [re.sub(r'\\item\s(.*)$', r'<li>\1</li>', line) for line in self.lines]
 
 
@@ -79,7 +84,7 @@ class Post(Block):
 
 if __name__ == '__main__':
 
-    post = Post('../latex_posts/art.tex')
+    post = Post('../latex_posts/article.tex')
 #    print(''.join(post.lines))
 
     print(''.join(post.process_block()))
